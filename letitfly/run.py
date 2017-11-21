0ea18ce36553b12eec5366b01a15544fe8de9638
YukiKuroshima <yukikuro1993@gmail.com>
2017-11-21 00:14:03 -0800

    Try SSLify

--- letitfly/run.py
+++ letitfly/run.py
@@ -1,6 +1,7 @@
 import os
 
 from app import create_app
+from flask_sslify import SSLify
 
 # Getting configuration from .env file
 config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
@@ -9,4 +10,6 @@ app = create_app(config_name)
 if __name__ == '__main__':
     # Heroku needs this
     port = int(os.environ.get('PORT', 5000))
+    if 'DYNO' in os.environ:
+        sslify = SSLify(app)
     app.run(host='0.0.0.0', port=port)

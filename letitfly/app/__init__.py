38c343a1f3a6080329f62e49efdd1b0cd29af19c
Christine Le <justpeachiii@yahoo.com>
2017-11-20 19:12:29 -0800

    Take out redirect to register when registration fails

--- letitfly/app/__init__.py
+++ letitfly/app/__init__.py
@@ -120,10 +120,10 @@ def create_app(config_name):
                 # access_token = temp_user.generate_token(temp_user.user_id)
 
                 session['email'] = request.form.get('email')
-                return redirect('request')
+                return redirect('auth')
             except Exception as e:
                 content = {'err': 'All fields must be filled out and email must be unique'}
-                return render_template('register.html', content=content)
+                # return render_template('register.html', content=content)
             '''
             except exc.OperationalError as e:
                 # SQLalchemy missing value

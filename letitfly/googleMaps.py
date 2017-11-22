cccf163f7646d5a0f374e8440b9906f11ad81520
domuchu <lau.marcus76@gmail.com>
2017-11-21 17:23:21 -0800

    Added correct template to run with

--- letitfly/googleMaps.py
+++ letitfly/googleMaps.py
@@ -54,7 +54,7 @@ def fullmap():
             }
         ],
     )
-    return render_template('directory.html', fullmap=fullmap)
+    return render_template('passB orig.html', fullmap=fullmap)
 
 
 if __name__ == "__main__":
